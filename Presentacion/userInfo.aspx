55649ff33e578c9cb22ac6a64d3cee2057de1307
andreagori <andrearivassg@gmail.com>
2024-12-10 02:28:45 -0800

    userInfo page backend completed

--- Presentacion/userInfo.aspx
+++ Presentacion/userInfo.aspx
@@ -1,17 +1,18 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/home.Master" AutoEventWireup="true" CodeBehind="userInfo.aspx.cs" Inherits="Presentacion.userInfo" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <main>
         <header>
             <asp:Label runat="server" ID="userName">A</asp:Label>
-            <img src="" id="userImage" />
+            <img src="" id="userImage" runat="server" />
         </header>
 
-        <asp:Label runat="server" ID="correo">A</asp:Label>
+        <asp:Label runat="server" ID="correo"></asp:Label>
         <br />
-        <asp:Label runat="server" ID="celular">123</asp:Label>
+        <asp:Label runat="server" ID="celular"></asp:Label>
         <br />
-        <asp:Label runat="server" ID="direccion">ASD</asp:Label>
+        <asp:Label runat="server" ID="direccion"></asp:Label>
     </main>
 </asp:Content>
--- Presentacion/userInfo.aspx.cs
+++ Presentacion/userInfo.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,11 +8,26 @@ using System.Web.UI.WebControls;
 
 namespace Presentacion
 {
-	public partial class userInfo : System.Web.UI.Page
-	{
-		protected void Page_Load(object sender, EventArgs e)
+    public partial class userInfo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
 		{
+            if (Session["Owner"] == null)
+            {
+                Response.Redirect("~/login.aspx");
+            }
+            else
+            {
+                E_Owner owner = (E_Owner)Session["Owner"];
+                userImage.Attributes["src"] = "assets/avatares/PetCare_Avatares/" + owner.ImgPerfil.ToString();
 
-		}
+                userName.Text = owner.Name; 
+                correo.Text = owner.Email;   
+                celular.Text = owner.CellPhone;  
+                direccion.Text = owner.Address; 
+
+
+            }
+        }
 	}
 }
