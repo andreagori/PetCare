1e326f7d3b4415c3d7bd9de1f2948e1816c17dd5
Anrago <141792891+Anrago@users.noreply.github.com>
2024-12-09 21:18:47 -0800

    Se extraen y se muestran los datos de los padecimientos

--- Presentacion/medicalCard.aspx.designer.cs
+++ Presentacion/medicalCard.aspx.designer.cs
@@ -60,12 +60,12 @@ namespace Presentacion
         protected global::System.Web.UI.WebControls.Label weight;
 
         /// <summary>
-        /// Control RpDates.
+        /// Control RpIllness.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater RpDates;
+        protected global::System.Web.UI.WebControls.Repeater RpIllness;
     }
 }
