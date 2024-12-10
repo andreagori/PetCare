1d20a1258bd0f22fba670faafe21aa7d3e57defe
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2024-12-10 00:14:19 -0800

    User

--- Presentacion/userInfo.aspx
+++ Presentacion/userInfo.aspx
@@ -2,4 +2,16 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <main>
+        <header>
+            <asp:Label runat="server" ID="userName">A</asp:Label>
+            <img src="" id="userImage" />
+        </header>
+
+        <asp:Label runat="server" ID="correo">A</asp:Label>
+        <br />
+        <asp:Label runat="server" ID="celular">123</asp:Label>
+        <br />
+        <asp:Label runat="server" ID="direccion">ASD</asp:Label>
+    </main>
 </asp:Content>
--- Presentacion/userInfo.aspx.designer.cs
+++ Presentacion/userInfo.aspx.designer.cs
@@ -13,5 +13,41 @@ namespace Presentacion
 
     public partial class userInfo
     {
+
+        /// <summary>
+        /// Control userName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label userName;
+
+        /// <summary>
+        /// Control correo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label correo;
+
+        /// <summary>
+        /// Control celular.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label celular;
+
+        /// <summary>
+        /// Control direccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label direccion;
     }
 }
